6541171b0671943e3dbb70679af97ad23d79c472
Vladislav <voloshanowky@yandex.ru>
2023-02-11 21:02:15 +0300

    вывели значения в консоль

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './components/models/product';
+import { ProductsService } from './servises/products.service';
+// import { products as data } from './components/data/product';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tour-of-heroes';
+  products: IProduct[] = []
+
+  constructor(private productsService: ProductsService) {}
+  ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => {
+      console.log(products);
+    })
+  }
+
 }
+
---
+++ src/app/servises/products.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ProductsService {
+    constructor(private http: HttpClient) {
+    }
+
+    getAll() {
+      return this.http.get('https://vue-study.skillbox.cc/api/products')
+    }
+}
